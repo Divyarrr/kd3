c3ff6de664e80a953abfb8f22e7a70812ac1dbae
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 08:11:41 +0530

    Create 27.C

---
+++ 27.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a;
+    scanf("%c",&a);
+    if(isdigit(a))
+        {
+         printf("yes");
+        }
+    else
+       {
+        printf("no");
+       }
+    getch();
+}
