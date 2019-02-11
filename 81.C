e49be604ef8d8d270ffbee6ac20fefa5ce4eec95
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 10:37:14 +0530

    Create 81.C

---
+++ 81.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int tc=3;
+    int i=0,c[100];
+    while(tc)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        
+            c[i]=b-a;
+            i++;
+       tc--; 
+    }
+    int k;
+    for(k=0;k<i;k++)
+    {
+        printf("%d\n",c[k]);
+    }
+    getch();
+}
