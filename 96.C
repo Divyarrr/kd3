e37c94fd2352d45c06d877841beed2c1bc03fbb3
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 10:55:42 +0530

    Create 96.C

---
+++ 96.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int C,A,B,i,b[10],sum=0;
+    scanf("%d%d%d",&A,&B,&C);
+    for(i=0;i<C;i++)
+    {
+        b[i]=A+i*B;
+    }
+    for(i=0;i<C;i++)
+    {
+        sum=sum+b[i];
+    }
+    printf("%d",sum);
+    getch();
+}
