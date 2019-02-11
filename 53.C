258fdd70274bf505d16f264756e5328e52c08659
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 09:50:30 +0530

    Create 53.C

---
+++ 53.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,sum=0,i=0,rem;
+    scanf("%d",&a);
+    i=0;
+    while(a)
+    {
+        rem=a%10;
+        a=a/10;
+       sum=sum+rem;
+        i++;
+    }
+  printf("%d",sum);
+    getch();
+}
