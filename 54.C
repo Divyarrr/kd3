2970dc45ec93b4d64cad91206b32af6407a6004c
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 09:51:47 +0530

    Create 54.C

---
+++ 54.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    scanf("%d",&i);
+        if (i>0)
+        {
+        if(i%2==0)
+        printf("%d",i);
+        else
+        printf("%d",i-1);
+        }
+    getch();
+}
