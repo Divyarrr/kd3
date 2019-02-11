d748822ad40c36521b6536eeafa9e107b2372883
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-11 07:58:27 +0530

    Create 12.C

---
+++ 12.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a,sum=0,rem,temp;
+    scanf("%d",&a);
+    temp=a;
+    if(a<=1000)
+    {
+    while(a)
+    {
+        rem=a%10;
+        sum=sum*10+rem;
+        a=a/10;
+    }
+    }
+    if(temp==sum)
+    printf("yes");
+    else
+    printf("not");
+    getch();
+}
