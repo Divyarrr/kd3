4a69a575597ba1beb6f2041c48261a9526e92af5
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-12 21:23:22 +0530

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+  int a,l;
+  scanf("%d",&a);
+  l=a/2;
+  if(a%2==0)
+  {
+      if(l%2==0)
+  {
+    while(l%2==0)
+    {
+      l=l/2;
+     }   
+    printf("%d",l);
+  }
+  else
+  {
+      printf("%d",l);
+  }
+  }
+  else
+  {
+      printf("%d",a);
+  }
+  getch();
+}
