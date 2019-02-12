5694d7f925a52c9269b663ce2aa30d5886845bed
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-02-12 21:26:50 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,l=0;
+    scanf("%d%d",&a,&b);
+    int t;
+    t=b;
+    while(t)
+    {
+        t=t/10;
+        l++;
+    }
+    for(i=0;i<l;i++)
+    {
+      a=a*10;  
+    }
+    printf("%d",a+b);
+    getch();
+}
